16478de69b28b8f7883f96ef13c0424f4db76137
YI ZHUANG <urlikeahurricane@gmail.com>
2018-01-08 23:00:04 +0200

    new

--- server.js
+++ server.js
@@ -8,6 +8,16 @@ app.use(cors());
 
 require('./route')(app);
 
+if (process.env.NODE_ENV === "production") {
+  // express will serve up production assets.
+  app.use(express.static("client/build"));
+
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
